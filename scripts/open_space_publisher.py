bfe2281b57f5277b82c542c31e774ba7c46d6b05
ChenZhenY <1207261079@qq.com>
2022-02-10 18:05:27 -0500

    Q5 custom msg

--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -3,31 +3,37 @@
 from random import random
 import rospy
 import random
+from ros_exercises.msg import OpenSpace
 from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
 
-class OpenSpace ():
+class OpenSpacePub ():
     def __init__(self):
         self.value = 0
+        self.space_msg = OpenSpace()
         rospy.init_node('open_space_publisher', anonymous=True)
-        self.pub_dist = rospy.Publisher('/open_space/distance',Float32,queue_size=10)
-        self.pub_angle = rospy.Publisher('/open_space/angle',Float32,queue_size=10)
+        # self.pub_dist = rospy.Publisher('/open_space/distance',Float32,queue_size=10)
+        # self.pub_angle = rospy.Publisher('/open_space/angle',Float32,queue_size=10)
+        self.pub_space = rospy.Publisher('/open_space',OpenSpace,queue_size=10)
         rospy.Subscriber('/fake_scan', LaserScan, self.listener)
 
     def listener(self, msg):
         self.range = msg.ranges
         self.max_range = max(self.range)
         self.max_angle = self.range.index(self.max_range) # only get first occurence
-        self.pub_dist.publish(self.max_range)
-        self.pub_angle.publish(self.max_angle)
-        rospy.loginfo(self.max_angle)
+        self.space_msg.angle = self.max_angle
+        self.space_msg.distance = self.max_range
+        self.pub_space.publish(self.space_msg)
+        # self.pub_dist.publish(self.max_range)
+        # self.pub_angle.publish(self.max_angle)
+        # rospy.loginfo(self.max_angle)
 
     def spin(self):
         rate = rospy.Rate(20)
         rospy.spin()
 
 if __name__ == '__main__':
-    OpenSpace1 = OpenSpace()
+    OpenSpace1 = OpenSpacePub()
     try:
         OpenSpace1.spin()
     except rospy.ROSInterruptException:
